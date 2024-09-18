ea0a91af002d2441a6ec7048409dbb41deb2cbdc
shlomn <shlomo@anymaint.com>
2024-09-18 20:28:07 +0300

    eslint for now

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
+import eslintReact from 'eslint-plugin-react';
 import eslintReactHooks from 'eslint-plugin-react-hooks';
 import eslintReactRefresh from 'eslint-plugin-react-refresh';
 import prettierPlugin from 'eslint-plugin-prettier';
@@ -10,14 +10,20 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 
 export default tseslint.config(
+  react.configs.recommended,
+
   {
     plugins: {
       '@typescript-eslint': tseslint.plugin,
-       react,
+      'react': eslintReact,
+      
       'react-hooks': eslintReactHooks,
       'react-refresh': eslintReactRefresh,
       prettier: prettierPlugin,
     },
+  },
+  {
+
   },
   {
     ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
@@ -32,10 +38,7 @@ export default tseslint.config(
         ...globals.es2020,
       },
       parserOptions: {
-        project: ['tsconfig.json', 'tsconfig.node.json','tsconfig.app.json'],
-        ecmaFeatures: {
-          jsx: true,
-        },
+        project: ['tsconfig.json', 'tsconfig.node.json','tsconfig.app.json']
       }
     },
   },
